04b7adb7a4beedf13c4392993b64e2744fe5f667
Justin Guertin <justin.guertin@gmail.com>
2019-10-18 10:26:45 -0700

    Update the library for JDK8 compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 group = "com.vituary"
 version = "0.1.0"
 
@@ -6,14 +8,20 @@ plugins {
     maven
 }
 
-
 repositories {
     mavenLocal()
     jcenter()
 }
 
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
